b5ef3e629437e7878fb58297340f3115e8ac0e04
diixo <anatolii.podoba@gmail.com>
2023-05-30 09:11:46 +0300

    updated

--- main.py
+++ main.py
@@ -71,10 +71,10 @@ def softmax_crossentropy_with_logits(logits, reference_answers):
 
 def grad_softmax_crossentropy_with_logits(logits, reference_answers):
     """Compute crossentropy gradient from logits[batch, n_classes] and ids of correct answers"""
-    ones_for_answers = np.zeros_like(logits)
-    ones_for_answers[np.arange(len(logits)), reference_answers] = 1
+    one_hot = np.zeros_like(logits)
+    one_hot[np.arange(len(logits)), reference_answers] = 1
 
     softmax = np.exp(logits) / np.exp(logits).sum(axis=-1, keepdims=True)
 
-    return (- ones_for_answers + softmax) / logits.shape[0]
+    return (- one_hot + softmax) / logits.shape[0]
 
